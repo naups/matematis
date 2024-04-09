e9005b9b876f3b958f5d2cb81dea542a8aa67750
Ahmad Nauval Syahputra <anvalputra@gmail.com>
2024-04-09 16:06:10 +0700

    matematis: test running node package

---
+++ test.js
@@ -0,0 +1,8 @@
+// test.js
+
+const { tambah, kurang, kali, bagi } = require('./index');
+
+console.log(tambah(5, 3)); // Output: 8
+console.log(kurang(5, 3)); // Output: 2
+console.log(kali(5, 3));   // Output: 15
+console.log(bagi(6, 3));   // Output: 2
